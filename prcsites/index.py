ba142155410087e3fed59884bd9d1b34ecffd721
Join Smith <lbp@liuboping.com>
2018-06-14 14:12:33 +0800

    init

--- prcsites/index.py
+++ prcsites/index.py
@@ -1,33 +1,39 @@
+# coding=utf-8
+from __future__ import unicode_literals
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from browsermobproxy import Server
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 import re
-
-server = Server(path='/opt/browsermob-proxy-2.1.4/bin/browsermob-proxy', options={'port': 8301})
-server.start()
 
 # global prc_sites
 prc_sites = {}
 clicked = {}
+loaded_url = {}
 
-proxy = server.create_proxy()
-proxy.new_har()
 chrome_options = Options()
 # chrome_options.add_argument("--headless")
-chrome_options.add_argument('--proxy-server={host}:{port}'.format(host='localhost', port=proxy.port))
+# driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=chrome_options)
 
-driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=chrome_options)
-driver.set_page_load_timeout(30)
+firefox_options = Options()
+driver = webdriver.Firefox(executable_path='geckodriver', firefox_options=firefox_options)
+
+driver.set_page_load_timeout(5)
 
 
 def get_root_domain(url):
-    r = re.findall(':\/\/([\w\.]+\.)?(\w+\.\w+)', url)
+    r = re.findall(':\/\/([\w\.]+\.)?(\w+\.[a-zA-Z]{2,})', url)
     for m in r:
-        if len(m) == 2:
+        if len(m) == 2 and m[1] and 'google' not in m[1]:
             return m[1]
         else:
-            print(m)
-            raise m
+            print('error get_root_domain ', m)
+            # raise m
+
+
+def get_domain(url):
+    r = re.findall(':\/\/([\w\.]+\.[a-zA-Z]{2,})', url)
+    if r:
+        return r[0]
 
 
 def add_domain(url):
@@ -41,22 +47,33 @@ def add_domain(url):
 
 
 def add_link(url):
-    if url.startswith('http') and not url.endswith('#') and not url.endswith('apk') and not url.endswith(
-            'exe') and link not in clicked:
-        add_domain(link)
-        try:
-            driver.get(link)
-            clicked[url] = 1
-            print('add link %s' % url)
-            return True
-        except Exception as e:
-            print(e)
+    if url.startswith('http') and 'google' not in url:
+        add_domain(url)
+        full_domain = get_domain(url)
+        if full_domain and full_domain not in clicked:
+            try:
+                if not url.endswith('apk') and not url.endswith('exe'):
+                    driver.get(url)
+                    clicked[full_domain] = 1
+                    print('full domain %s , add link %s ' % (full_domain, url))
+                    timings = driver.execute_script("return window.performance.getEntries();")
+                    for pfm in timings:
+                        if 'initiatorType' in pfm:
+                            # print('loaded resource %s' % pfm['name'])
+                            add_domain(pfm['name'])
+                            tmp_full_domain = get_domain(pfm['name'])
+                            loaded_url[tmp_full_domain] = 1
+                    return True
+            except Exception as e:
+                print(e)
+                # driver.refresh()
 
     return False
 
 
 try:
-    driver.get("https://www.hao123.com")
+    add_link("https://www.hao123.com")
+
     # driver.get("http://www.people.com.cn/")
     # ':\/\/(www[0-9]?\.)?(.[^/:]+)'
     elems = driver.find_elements_by_xpath("//a[@href]")
@@ -75,21 +92,21 @@ try:
             elems2 = driver.find_elements_by_xpath("//a[@href]")
             for elem2 in elems2:
                 try:
-                    p2_link_list.append(elem2.get_attribute("href"))
+                    if elem2.text == u'更多>>':
+                        p2_link_list.append(elem2.get_attribute("href"))
                 except Exception as e:
                     print(e)
             for link2 in p2_link_list:
                 if add_link(link2):
                     pass
             # break
-    all_requests = [entry['request']['url'] for entry in proxy.har['log']['entries']]
-    for l in all_requests:
-        add_link(l)
+    # all_requests = [entry['request']['url'] for entry in proxy.har['log']['entries']]
+    for l in loaded_url:
+        add_domain(l)
 except Exception as e:
     print(e)
 finally:
     driver.quit()
-    server.stop()
     with open('../.env/prc-sites.txt', 'w') as f:
         f.writelines([domain + "\n" for domain in prc_sites.keys()])
 
